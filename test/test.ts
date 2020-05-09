9c816ea1eda27b09b58fb590fc38c29ca81b4c85
sascha <sascha@jaschke.it>
2020-05-09 16:47:08 +0200

    add small test

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,17 @@
 import {expect} from 'chai';
 import 'mocha';
+import {requireAuth} from '../src/controllers/v0/feed/routes/feed.router';
 
-describe('test', () =>{
-    it('should return true',  async () => {
-        expect(true).to.equal(true);
+const MockExpressRequest = require('mock-express-request');
+const MockExpressResponse = require('mock-express-response');
+
+
+describe('requireAuth test', () => {
+    it('test no headers', async () => {
+        let request = new MockExpressRequest();
+        let response = new MockExpressResponse();
+        requireAuth(request, response, () => {
+        });
+        expect(response.statusCode).to.equal(401);
     });
 });
